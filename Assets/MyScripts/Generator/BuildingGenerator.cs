41376d1ae91e8b7fac8772ca72c2a831aa216a3d
chengpatrick <qq240766532@gmail.com>
2021-11-29 21:51:56 -0500

    updated town details

--- Assets/MyScripts/Generator/BuildingGenerator.cs
+++ Assets/MyScripts/Generator/BuildingGenerator.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class BuildingGenerator : MonoBehaviour
 {
-    public GameObject leftBuiilding;
-    public GameObject rightBuilding;
+    // public GameObject leftBuiilding;
+    // public GameObject rightBuilding;
+
+    public GameObject town;
     public GameObject path;
     public GameObject character;
 
@@ -17,52 +19,66 @@ public class BuildingGenerator : MonoBehaviour
     void Start()
     {
         count = 0;
-        leftBuiilding.name="LeftBuilding count:"+count;
-        rightBuilding.name="RightBuilding count:"+count;
+        // leftBuiilding.name="LeftBuilding count:"+count;
+        // rightBuilding.name="RightBuilding count:"+count;
+        town.name="Town count:"+count;
         path.name="Path count:"+count;
 
         foreach (Transform child in path.transform.GetChild(0).transform) {
+            if(child.name.ToString()!="Plane")
              GameObject.Destroy(child.gameObject);
         }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // detech if player has reached position for spawning next tiles
-        if (character.transform.position.z > count * 120 + 30)
+        if (character.transform.position.z > count * 120)
         {
             // increment counter until player gets to next position to spawn
             count++;               
-
+          
             // clone left and right buildings
-            var newLeftBuilding=Instantiate(leftBuiilding, new Vector3(leftBuiilding.transform.position.x, 0,count * 120), leftBuiilding.transform.rotation);
-            var newRightBuilding=Instantiate(rightBuilding, new Vector3(rightBuilding.transform.position.x, 0, count * 120), rightBuilding.transform.rotation);
+            // var newLeftBuilding=Instantiate(leftBuiilding, new Vector3(leftBuiilding.transform.position.x, 0,count * 120), leftBuiilding.transform.rotation);
+            // var newRightBuilding=Instantiate(rightBuilding, new Vector3(rightBuilding.transform.position.x, 0, count * 120), rightBuilding.transform.rotation);
+            var newTown=Instantiate(town, new Vector3(town.transform.position.x, 0, count * 120), town.transform.rotation);
 
             // change name of new clone
-            newLeftBuilding.name="LeftBuilding count:"+count;
-            newRightBuilding.name="RightBuilding count:"+count;
+            // newLeftBuilding.name="LeftBuilding count:"+count;
+            // newRightBuilding.name="RightBuilding count:"+count;
+            newTown.name="Town count:"+count;
           
             //if (count >= 2)
             //{
                 // clone path
                 // var newPath=Instantiate(path, new Vector3(path.transform.position.x, 0, (count-1) * 110 + 150), path.transform.rotation);
-                var newPath=Instantiate(path, new Vector3(path.transform.position.x, 0, (count)*120+50), path.transform.rotation);
+                var newPath=Instantiate(path, new Vector3(path.transform.position.x, 0, (count)*120), path.transform.rotation);
 
                 // change name of new path
                 newPath.name="Path count:"+count;
 
+                // destory all children of new clone if it's not a plane
+                foreach (Transform child in newPath.transform) {
+                    if(child.name.ToString()!="Plane")
+                    GameObject.Destroy(child.gameObject);
+                }
+
                 // destory old path and building
-                GameObject leftBuildingToDestroy=GameObject.Find("LeftBuilding count:"+(count-2));
-                GameObject rightBuildingToDestroy=GameObject.Find("RightBuilding count:"+(count-2));
+                // GameObject leftBuildingToDestroy=GameObject.Find("LeftBuilding count:"+(count-2));
+                // GameObject rightBuildingToDestroy=GameObject.Find("RightBuilding count:"+(count-2));
+                GameObject townToDestroy=GameObject.Find("Town count:"+(count-2));
                 GameObject pathToDestroy=GameObject.Find("Path count:"+(count-2));
-                Destroy(leftBuildingToDestroy);
-                Destroy(rightBuildingToDestroy);
+                // Destroy(leftBuildingToDestroy);
+                // Destroy(rightBuildingToDestroy);
+                Destroy(townToDestroy);
                 Destroy(pathToDestroy);
 
                 // set new building and path to clone 
-                leftBuiilding=GameObject.Find("LeftBuilding count:"+(count-1));
-                rightBuilding=GameObject.Find("RightBuilding count:"+(count-1));
+                // leftBuiilding=GameObject.Find("LeftBuilding count:"+(count-1));
+                // rightBuilding=GameObject.Find("RightBuilding count:"+(count-1));
+                town=GameObject.Find("Town count:"+(count-1));
                 path=GameObject.Find("Path count:"+(count-1));
             //}
             // else
--- Assets/MyScripts/Generator/ObstacleGenerator.cs
+++ Assets/MyScripts/Generator/ObstacleGenerator.cs
@@ -7,10 +7,16 @@ public class ObstacleGenerator : MonoBehaviour
 
     public GameObject path;
     public BuildingGenerator buildingGenerator;
+
+    // count of checkpoints the character has reached
     private int count;
 
+    // List of cars to read from
     private string[] cars;
     private List<string> carList;
+
+    // Queue of car positions in the scene
+    private Queue<Vector3> carPos;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +27,6 @@ public class ObstacleGenerator : MonoBehaviour
                         "Car_4_Blue","Car_4_Green","Car_4_Purple","Car_4_Red","Car_4_Yellow","Car_4_Silver",
                         "Car_5_Blue","Car_5_Green","Car_5_Purple","Car_5_Red","Car_5_Yellow","Car_5_Silver",
                         "Car_6_Blue","Car_6_Green","Car_6_Purple","Car_6_Red","Car_6_Yellow","Car_6_Silver",
-                        "Truck_1_Blue","Truck_1_Green","Truck_1_Purple","Truck_1_Red","Truck_1_Yellow","Truck_1_Silver",
                         "Truck_2_Blue","Truck_2_Green","Truck_2_Purple","Truck_2_Red","Truck_2_Yellow","Truck_2_Silver",
                         "Policecar"
                         };
@@ -32,6 +37,8 @@ public class ObstacleGenerator : MonoBehaviour
         List<string> busList = new List<string>(bus);
 
         count = buildingGenerator.count;
+
+        carPos = new Queue<Vector3>();
     }
 
     // Update is called once per frame
@@ -44,40 +51,72 @@ public class ObstacleGenerator : MonoBehaviour
     {
         // choose random car in list to clone
         int ran = Random.Range(0, carList.Count);
-        // number of objects to generate, min 3 max 5
-        int generateAmount = Random.Range(4, 7);
+        // number of objects to generate, min 4 max 5
+        int generateAmount = Random.Range(4, 6);
 
         // update count from BuildingGenerator
         count = buildingGenerator.count;
 
+        // if count>6 then double car obstacles
+        int double_ob = 1;
+        if (count > 0) double_ob = 2;
+
         for (int i = 0; i < generateAmount; i++)
         {
-            ran = Random.Range(0, carList.Count);
-
-            // random position between three lanes, 0 left 1 middile 2 right
-            int ranLane = Random.Range(0, 3);
-
-            // clone object from prefab file and assign it to newObject
-            GameObject newObject = Instantiate((GameObject)LoadPrefabFromFile(carList[ran]), new Vector3(-2.5f + ranLane * 2.5f, 0.8f, count * 120 + i * 20 - 60), Quaternion.identity * Quaternion.Euler(0f, 90f, 0f));
-
-            // adjust height of different prefab
-            if (carList[ran].StartsWith("Truck"))
+            for (int k = 0; k < double_ob; k++)
             {
-                newObject.transform.position = new Vector3(-2.5f + ranLane * 2.5f, 2f, count * 120 + i * 20);
-            }
-            else if (carList[ran].StartsWith("Car_4"))
-            {
-                newObject.transform.position = new Vector3(-2.5f + ranLane * 2.5f, 1.3f, count * 120 + i * 20);
-            }
-            else if (carList[ran].StartsWith("Car_5"))
-            {
-                newObject.transform.position = new Vector3(-2.5f + ranLane * 2.5f, 1f, count * 120 + i * 20);
-            }
+                // remove old deleted car from queue
+                if (carPos.Count > 5) carPos.Dequeue();
 
-            // assign object to path as child
-            newObject.transform.parent = GameObject.Find("Path count:" + count).transform;
-            newObject.AddComponent<BoxCollider>();
+                ran = Random.Range(0, carList.Count);
+
+                // check if current pos has car already
+                bool hasCar = true;
+                // new car to be created
+                GameObject newObject = new GameObject();
+                Destroy(newObject);
+
+                while (hasCar)
+                {
+                    // random position between three lanes, 0 left 1 middile 2 right
+                    int ranLane = Random.Range(0, 3);
+
+                    // clone object from prefab file and assign it to newObject
+                    newObject = Instantiate((GameObject)LoadPrefabFromFile(carList[ran]), new Vector3(-2.5f + ranLane * 2.5f, 0.8f, count * 120 + i * 20 - 90), Quaternion.identity * Quaternion.Euler(0f, 90f, 0f));
+
+                    // adjust height of different prefab
+                    if (carList[ran].StartsWith("Truck"))
+                    {
+                        newObject.transform.position = new Vector3(-2.5f + ranLane * 2.5f, 1.8f, count * 120 + i * 20 - 90);
+                    }
+                    else if (carList[ran].StartsWith("Car_4"))
+                    {
+                        newObject.transform.position = new Vector3(-2.5f + ranLane * 2.5f, 1.3f, count * 120 + i * 20 - 90);
+                    }
+                    else if (carList[ran].StartsWith("Car_5"))
+                    {
+                        newObject.transform.position = new Vector3(-2.5f + ranLane * 2.5f, 1f, count * 120 + i * 20 - 90);
+                    }
+
+                    // check if queue has this pos, if yes then regenerate
+                    if (!carPos.Contains(new Vector3(newObject.transform.position.x, 2, newObject.transform.position.z)))
+                    {
+                        hasCar = false;                       
+                    }else{
+                        // destroy old car
+                        Destroy(newObject);
+                    }
+                }
+
+                // add position of new generated car to queue
+                carPos.Enqueue(new Vector3(newObject.transform.position.x, 2, newObject.transform.position.z));
+
+                // assign object to path as child
+                newObject.transform.parent = GameObject.Find("Path count:" + count).transform;
+                newObject.AddComponent<BoxCollider>();
+            }
         }
+
 
     }
 
