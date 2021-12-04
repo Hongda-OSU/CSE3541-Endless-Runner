a783ba862f8bca08224bf8d27a60b92f738a7a8b
Xiaoliang Chen <70415185+Xiaoliang020@users.noreply.github.com>
2021-12-04 18:08:22 -0500

    Few updates

--- Assets/MyScripts/CoinsGenerator/CoinGenerator.cs
+++ Assets/MyScripts/CoinsGenerator/CoinGenerator.cs
@@ -1,66 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using UnityEngine;
 
-namespace EndLessRunner
-{
-    public class CoinGenerator : MonoBehaviour
-    {
-        [SerializeField]
-        public int minNum = 5;
+//namespace EndLessRunner
+//{
+//    public class CoinGenerator : MonoBehaviour
+//    {
+//        [SerializeField]
+//        public int minNum = 5;
 
-        [SerializeField]
-        public int maxNum = 10;
+//        [SerializeField]
+//        public int maxNum = 10;
 
-        [SerializeField]
-        private Transform Parent = null;
+//        [SerializeField]
+//        private Transform Parent = null;
 
-        private float leftPos = -2.52f;
-        private float midPos = 0f;
-        private float rightPos = 2.48f;
+//        private float leftPos = -2.52f;
+//        private float midPos = 0f;
+//        private float rightPos = 2.48f;
 
-        public GameObject coins;
+//        public GameObject coins;
 
-        private int[] availablePos = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//        private int[] availablePos = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            int numberOfCoins = Random.Range(5, 10);
-            for (int i = 0; i < numberOfCoins; i++)
-            {
-                int x = Random.Range(-1, 2);
-                float xPosition = midPos;
-                int zPosition = Random.Range(1, 10);
-                while (!availablePos.Contains(zPosition))
-                {
-                    zPosition = Random.Range(1, 10);
-                }
-                availablePos = availablePos.Where(val => val != zPosition).ToArray();
+//        // Start is called before the first frame update
+//        void Start()
+//        {
+//            int numberOfCoins = Random.Range(5, 10);
+//            for (int i = 0; i < numberOfCoins; i++)
+//            {
+//                int x = Random.Range(-1, 2);
+//                float xPosition = midPos;
+//                int zPosition = Random.Range(1, 10);
+//                while (!availablePos.Contains(zPosition))
+//                {
+//                    zPosition = Random.Range(1, 10);
+//                }
+//                availablePos = availablePos.Where(val => val != zPosition).ToArray();
 
-                if (x == -1)
-                {
-                    xPosition = leftPos;
-                }
-                else if (x == 1)
-                {
-                    xPosition = rightPos;
-                }
-                if(zPosition != 2 && zPosition != 5 && zPosition != 7)
-                {
-                    Vector3 coinPosition = new Vector3(xPosition, coins.transform.position.y, zPosition * 10 + TileGenerator.generationCount * 110);
-                    GameObject.Instantiate<GameObject>(coins, coinPosition, coins.transform.rotation, Parent);
-                }
+//                if (x == -1)
+//                {
+//                    xPosition = leftPos;
+//                }
+//                else if (x == 1)
+//                {
+//                    xPosition = rightPos;
+//                }
+//                if(zPosition != 2 && zPosition != 5 && zPosition != 7)
+//                {
+//                    Vector3 coinPosition = new Vector3(xPosition, coins.transform.position.y, zPosition * 10 + TileGenerator.generationCount * 110);
+//                    GameObject.Instantiate<GameObject>(coins, coinPosition, coins.transform.rotation, Parent);
+//                }
                 
-            }
-        }
+//            }
+//        }
 
-        // Update is called once per frame
-        void Update()
-        {
+//        // Update is called once per frame
+//        void Update()
+//        {
 
-        }
-    }
+//        }
+//    }
 
-}
+//}
